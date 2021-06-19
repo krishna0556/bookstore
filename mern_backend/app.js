dd17414163fbf4c297d2f905a155e28b2854f091
krishna0556 <matireddysaikrishna@gmail.com>
2021-06-19 10:42:53 +0530

    first commit

---
+++ mern_backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const connectDB = require('./config/db');
+var cors = require('cors');
+
+// routes
+const books = require('./routes/api/books');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) =>
+    res.send('Successfully connected!')
+);
+
+// use Routes
+app.use('/api/books', books);
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () =>
+    console.log(`Server running on port ${port}`)
+);
